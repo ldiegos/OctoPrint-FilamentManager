d7adeae4aff1f056a5f257650f3b4471bdc17f7d
ldiegoes <ki@ki.es>
2022-10-30 00:46:58 +0200

    ff

--- setup.py
+++ setup.py
@@ -14,13 +14,12 @@ plugin_url = "https://github.com/OllisGit/OctoPrint-FilamentManager"
 plugin_license = "AGPLv3"
 plugin_requires = ["backports.csv>=1.0.5,<1.1",
                    "uritools>=2.1,<2.2",
-                   "SQLAlchemy>=1.1.15,<1.2",
-                   "python_requires": ">=3,<4"]
+                   "SQLAlchemy>=1.1.15,<1.2"]
 plugin_additional_data = []
 plugin_additional_packages = []
 plugin_ignored_packages = []
-additional_setup_parameters = {}
-
+#additional_setup_parameters = {}
+additional_setup_parameters = {"python_requires": ">=3,<4"}
 
 ########################################################################################################################
 
